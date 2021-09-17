cfdff503ff97e34b2c6a2c214eb3d239bdc9160a
Dmitry <sheklein.dima@gmail.com>
2021-09-17 22:41:32 +0300

    Добавил запрос данных по API

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import AppHeader from "./components/app-header/app-header";
 import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
 import BurgerConstructor from "./components/burger-constructor/burger-constructor";
-import {data as fakeData} from './utils/data.js';
 
 function App() {
   const [productsData, setProductsData] = useState([]);
