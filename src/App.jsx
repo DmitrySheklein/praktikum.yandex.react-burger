55063260d9a9592d68c01b0ccf83f19cddfb7ba6
Dmitry <sheklein.dima@gmail.com>
2021-09-17 22:36:41 +0300

    Создал CRA заготовку с фейковыми данными

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "./App.css";
-import AppHeader from "./components/app-header";
-import BurgerIngredients from "./components/burger-ingredients";
-import BurgerConstructor from "./components/burger-constructor";
+import AppHeader from "./components/app-header/app-header";
+import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "./components/burger-constructor/burger-constructor";
+import {data as fakeData} from './utils/data.js';
 
 function App() {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(()=>{
+    setProductsData(fakeData)
+  },[]);
+
   return (
     <div className="App">
       <AppHeader />
       <main className="container flex">
-        <BurgerIngredients />
-        <BurgerConstructor />
+        <BurgerIngredients productsData={productsData}/>
+        <BurgerConstructor productsData={productsData} />
       </main>
     </div>
   );
---
+++ src/components/burger-constructor/burger-constructor.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  ConstructorElement,
+  DragIcon,
+  Button,
+  CurrencyIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./burger-constructor.module.css";
+
+const BurgerConstructor = ({ productsData }) => {
+  const bunSampleObj = productsData.find(el => el.type === "bun");
+  const sampleIngredientsArr = productsData.filter(el => el.type !== "bun");
+
+  return (
+    <div className={`${styles.block} pt-25 pb-15 pl-4 pr-4`}>
+      {productsData.length && bunSampleObj ? (
+        <div className={`${styles.constructorList} mb-10`}>
+          <div className={`${styles.constructorListItem} pr-8 pl-6`}>
+            <ConstructorElement
+              type="top"
+              isLocked={true}
+              text={bunSampleObj.name}
+              price={bunSampleObj.price}
+              thumbnail={bunSampleObj.image}
+            />
+          </div>
+          <div className={`${styles.constructorSubList} pr-4 custom-scroll`}>
+            {sampleIngredientsArr &&
+              sampleIngredientsArr.map(product => (
+                <div
+                  className={`${styles.constructorSubListItem}`}
+                  key={product._id}
+                >
+                  <DragIcon type="primary" />
+                  <ConstructorElement
+                    text={product.name}
+                    price={product.price}
+                    thumbnail={product.image}
+                  />
+                </div>
+              ))}
+          </div>
+          <div className={`${styles.constructorListItem} pr-8 pl-6`}>
+            <ConstructorElement
+              type="bottom"
+              isLocked={true}
+              text={bunSampleObj.name}
+              price={bunSampleObj.price}
+              thumbnail={bunSampleObj.image}
+            />
+          </div>
+        </div>
+      ) : null}
+
+      <div className={`${styles.constructorTotal}`}>
+        <div className={`${styles.constructorTotalPrice} mr-10`}>
+          <p className="text text_type_digits-medium">610</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default BurgerConstructor;
